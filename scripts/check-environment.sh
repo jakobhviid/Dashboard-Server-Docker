0e8351726880c954f8136ec9e64d7daca7fafe11
Oliver Marco van Komen <olvan18@student.sdu.dk>
2020-07-15 10:29:52 +0200

    Output color fix

--- scripts/check-environment.sh
+++ scripts/check-environment.sh
@@ -4,11 +4,11 @@ DOCKER_SOCKET_PATH=${DOCKER_SOCKET_PATH:-/var/run/docker.sock}
 
 # Check if socket exists (-S)
 if [ ! -S "$DOCKER_SOCKET_PATH" ]; then
-    echo -e "\e[1;32mERROR - Docker socket has not been provided through volumes! \e[0m"
+    echo -e "\e[1;31mERROR - Docker socket has not been provided through volumes! \e[0m"
     exit 1
 fi
 
 if [ -z "$SERVER_NAME" ]; then
-    echo -e "\e[1;32mERROR - 'SERVER_NAME' has not been provided! \e[0m"
+    echo -e "\e[1;31mERROR - 'SERVER_NAME' has not been provided! \e[0m"
     exit 1
 fi
